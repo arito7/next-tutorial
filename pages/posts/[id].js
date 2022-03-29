da67f9c4bbbabe1a186eb62140a9ab572294ca37
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-29 13:10:30 +0900

    initial

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/Date';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  console.log(paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  console.log(postData);
+  return {
+    props: { postData },
+  };
+}
